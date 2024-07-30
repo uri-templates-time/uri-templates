1b13e8a3ee497a022eaf3b270c7b20605627d2a9
Jeremy Faden <faden@cottagesystems.com>
2024-07-30 09:50:48 -0500

    tweak version number

--- UriTemplatesJava/src/org/hapiserver/URITemplate.java
+++ UriTemplatesJava/src/org/hapiserver/URITemplate.java
@@ -39,7 +39,7 @@ public class URITemplate {
        
     private static final Logger logger= Logger.getLogger("hapiserver.uritemplates");
     
-    public static String VERSION= "20240705.1";
+    public static String VERSION= "20240730.1";
     
     public static String getVersion() {
         return VERSION;
